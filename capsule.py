8af99b22026e653434188ea43cadb7134d35a97c
Kuroyukihime000 <mishutka2282@tutamail.com>
2025-04-02 13:25:14 +0300

    dsf

---
+++ capsule.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class BigmacStats:
+    def __init__(self, df):
+        self.df = df
+
+    def
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+df = pd.read_csv("BigmacPrice.csv") 
+bigmac_analyzer = BigMacStats(df)
+bigmac_analyzer.display_data()
+bigmac_analyzer.show_statistics()
+bigmac_analyzer.plot_histogram()
---
+++ main.py
@@ -0,0 +1,9 @@
+from log import CAPS
+from log import LOW
+
+def main():
+    CAPS()
+    LOW()
+
+if __name__ == '__main__': #проверяем на прямой запуск
+    main() #запускаем функцию
